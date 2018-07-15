b3c92402af9532f2f6a6b36e697756a1408d5c4d
David Santamaria Lago <dsantamarialago@stubhub.com>
2018-07-16 00:57:11 +0200

    added final response with starship data

--- src/main/java/com/contract/provider/StarshipController.java
+++ src/main/java/com/contract/provider/StarshipController.java
@@ -17,7 +17,7 @@ public class StarshipController {
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36";
 
     @GetMapping("/ship")
-    public StarshipPojo getShip(@RequestParam(value = "id") Long id) {
+    public Starship getShip(@RequestParam(value = "id") Long id) {
 
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -31,9 +31,9 @@ public class StarshipController {
         logger.info("   :: NUMERO DE PILOTOS: " + numPilotos.toString());
 
         ArrayList<String> pilotos = new ArrayList<>();
-        if(numPilotos > 0) {
+        if (numPilotos > 0) {
             logger.info("   :: Buscando los nombres de los pilotos...");
-            for(int i = 0; i < numPilotos; i++){
+            for (int i = 0; i < numPilotos; i++) {
                 String nombrePiloto = getPilotName(starshipPojo.getBody().getPilots().get(i));
                 pilotos.add(nombrePiloto);
                 logger.info("   :: Añadido " + nombrePiloto + " a la lista de pilotos");
@@ -44,45 +44,62 @@ public class StarshipController {
         logger.info("   :: NUMERO DE PELICULAS: " + numPeliculas.toString());
 
         ArrayList<String> peliculas = new ArrayList<>();
-        if(numPeliculas > 0) {
+        if (numPeliculas > 0) {
             logger.info("   :: Buscando los títulos de las películas...");
-            for(int j = 0; j < numPeliculas; j++){
+            for (int j = 0; j < numPeliculas; j++) {
                 String titulo = getFilmTitle(starshipPojo.getBody().getFilms().get(j));
                 peliculas.add(titulo);
                 logger.info("   :: Añadido " + titulo + " a la lista de películas");
             }
         }
 
-        return starshipPojo.getBody();
+        Starship starship = buildStarship(starshipPojo.getBody(), pilotos, peliculas);
+        return starship;
     }
 
-     private String getPilotName(final String url){
+    private String getPilotName(final String url) {
 
-         HttpHeaders headers = new HttpHeaders();
-         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-         headers.add("user-agent", USER_AGENT);
-         HttpEntity request = new HttpEntity(headers);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", USER_AGENT);
+        HttpEntity request = new HttpEntity(headers);
 
-         final RestTemplate restTemplate = new RestTemplate();
-         ResponseEntity<PilotPojo> pilotPojoResponseEntity = restTemplate.exchange(url , HttpMethod.GET, request, PilotPojo.class);
+        final RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<PilotPojo> pilotPojoResponseEntity = restTemplate.exchange(url, HttpMethod.GET, request, PilotPojo.class);
 
-         return pilotPojoResponseEntity.getBody().getName();
+        return pilotPojoResponseEntity.getBody().getName();
 
-     }
-
-     private String getFilmTitle(final String url){
-         HttpHeaders headers = new HttpHeaders();
-         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-         headers.add("user-agent", USER_AGENT);
-         HttpEntity request = new HttpEntity(headers);
-
-         final RestTemplate restTemplate = new RestTemplate();
-         ResponseEntity<FilmPojo> filmPojoResponseEntity = restTemplate.exchange(url , HttpMethod.GET, request, FilmPojo.class);
-
-         return filmPojoResponseEntity.getBody().getTitle();
     }
 
+    private String getFilmTitle(final String url) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", USER_AGENT);
+        HttpEntity request = new HttpEntity(headers);
 
+        final RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<FilmPojo> filmPojoResponseEntity = restTemplate.exchange(url, HttpMethod.GET, request, FilmPojo.class);
+
+        return filmPojoResponseEntity.getBody().getTitle();
+    }
+
+    private Starship buildStarship(final StarshipPojo starshipPojo, final ArrayList<String> pilots, ArrayList<String> films) {
+        Starship starship = new Starship();
+        starship.setName(starshipPojo.getName());
+        starship.setModel(starshipPojo.getModel());
+        starship.setManufacturer(starshipPojo.getManufacturer());
+        starship.setCost_in_credits(starshipPojo.getCost_in_credits());
+        starship.setLength(starshipPojo.getLength());
+        starship.setMax_atmosphering_speed(starshipPojo.getMax_atmosphering_speed());
+        starship.setCrew(starshipPojo.getCrew());
+        starship.setPassengers(starshipPojo.getPassengers());
+        starship.setCargo_capacity(starshipPojo.getCargo_capacity());
+        starship.setStarship_class(starshipPojo.getStarship_class());
+        starship.setPilots(pilots);
+        starship.setFilms(films);
+
+        return starship;
+    }
 
 
 }
